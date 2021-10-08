2784bb2e06612903ab162fd49cc9f48fa98ec632
Gabriel <gabrielbittencourt57@gmail.com>
2021-10-08 17:10:32 -0300

    :truck: chore: Changed name of route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
 import { Switch, Route } from 'react-router-dom'
 
 import AvailableAnimes from '../pages/AvailableAnimes'
-import Dashboard from '../pages/Dashboard'
+import RandomAnimeQuote from '../pages/RandomAnimeQuote'
 
 const Routes = () => {
   return (
     <>
       <Switch>
-        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/" component={RandomAnimeQuote} />
       </Switch>
       <Switch>
         <Route path="/available" component={AvailableAnimes} />
