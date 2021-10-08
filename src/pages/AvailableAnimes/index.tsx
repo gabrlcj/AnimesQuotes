28756c38504404c36727f13d29efe6db3c07c04f
Gabriel <gabrielbittencourt57@gmail.com>
2021-10-08 17:07:13 -0300

    :sparkles: feat: Created the state to make pagination refactor: Make separated components to it

--- src/pages/AvailableAnimes/index.tsx
+++ src/pages/AvailableAnimes/index.tsx
@@ -1,23 +1,35 @@
 import { useEffect, useState } from 'react'
 import { AnimeList } from '../../@types/type'
+import AnimePagination from '../../components/AnimePagination'
 import Loading from '../../components/Loading'
+import Pagination from '../../components/Pagination'
 import api from '../../service/api'
-import { AnimesContainer, Container } from './style'
+import { Container } from './style'
 
 export default function AvailableAnimes() {
   const [allAnimes, setAllAnimes] = useState<AnimeList[]>([])
   const [loading, setLoading] = useState<boolean>(false)
-
-  async function listOfAnimes() {
-    setLoading(true)
-    const animes = await api.get('available/anime')
-    setAllAnimes(animes.data)
-    setLoading(false)
-  }
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [postPerPage] = useState<number>(50)
 
   useEffect(() => {
+    async function listOfAnimes() {
+      setLoading(true)
+      const animes = await api.get('available/anime')
+      setAllAnimes(animes.data)
+      setLoading(false)
+    }
+
     listOfAnimes()
   }, [])
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postPerPage
+  const indexOfFirstPost = indexOfLastPost - postPerPage
+  const currentPosts = allAnimes.slice(indexOfFirstPost, indexOfLastPost)
+
+  // Change page
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber)
 
   return (
     <Container>
@@ -25,15 +37,15 @@ export default function AvailableAnimes() {
       {loading === true ? (
         <Loading />
       ) : (
-        <AnimesContainer>
-          {allAnimes
-            .map((anime) => (
-              <li key={Math.random()}>
-                <strong>{anime}</strong>
-              </li>
-            ))
-            .slice(0, 100)}
-        </AnimesContainer>
+        <>
+          <Pagination
+            postsPerPage={postPerPage}
+            totalPosts={allAnimes.length}
+            paginate={paginate}
+            currentPage={currentPage}
+          />
+          <AnimePagination allAnimes={currentPosts} loading={loading} />
+        </>
       )}
     </Container>
   )
